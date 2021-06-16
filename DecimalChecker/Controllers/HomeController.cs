cabc8e869aef77e59fef90fe270bd81d1f88378d
Gerhard Gehrer <g.gehrer@hotmail.com>
2021-06-16 11:26:09 +0200

    Add project files.

---
+++ DecimalChecker/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using DecimalChecker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DecimalChecker.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private static readonly List<DecimalNumber> numbers = new();
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View("IndexNumbers", numbers);
+		}
+
+		public IActionResult Create()
+		{
+			return View(new DecimalNumber());
+		}
+
+		[HttpPost]
+		public IActionResult Create(DecimalNumber model)
+		{
+			if (string.IsNullOrEmpty(model.ErrorMessage))
+			{
+				numbers.Add(model);
+			}
+			return string.IsNullOrEmpty(model.ErrorMessage) ? RedirectToAction("Index") : View(model);
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ DecimalChecker/Models/DecimalNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DecimalChecker.Models
+{
+	public class DecimalNumber
+	{
+		private string input;
+		[RegularExpression(@"^(-)?\d*([\.\,]\d+)?$")]
+		public string Input 
+		{ 
+			get => input;
+			set
+			{
+				input = value;
+
+				if (input != null && Decimal.TryParse(input, out decimal dec))
+				{
+					ErrorMessage = string.Empty;
+					Number = dec;
+				}
+				else
+				{
+					ErrorMessage = "Invalid number format";
+					Number = default;
+				}
+			}
+		}
+		public decimal Number { get; set; }
+		public string ErrorMessage { get; private set; }
+	}
+}
